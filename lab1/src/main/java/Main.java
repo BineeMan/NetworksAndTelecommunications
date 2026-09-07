822d691d7f61dc377f628b564ed351ace080c240
BineeMan <herobrine0200@gmail.com>
2026-09-07 23:24:45 +0700

    t

--- lab1/src/main/java/Main.java
+++ lab1/src/main/java/Main.java
@@ -1,12 +1,11 @@
 import java.io.IOException;
 import java.net.*;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.nio.channels.DatagramChannel;
+import java.util.*;
 
 public class Main {
     static int port = 9876;
+
     public static void main(String[] args) throws Exception {
         String groupIpAddress = "";
         InetAddress groupAddress = InetAddress.getByName(groupIpAddress);
@@ -15,21 +14,41 @@ public class Main {
             System.out.println("Multicast address not supported");
             return;
         }
+        run(groupAddress);
     }
 
-    private NetworkInterface findSuitableInterface(InetAddress groupAddress) throws SocketException {
-        Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+    private static NetworkInterface findSuitableInterface(InetAddress groupAddress) throws SocketException {
+        List<NetworkInterface> interfaces = Collections.list(NetworkInterface.getNetworkInterfaces());
 
         boolean isIPv6 = groupAddress instanceof Inet6Address;
 
-        while (interfaces.hasMoreElements()) {
-            Networ
+        for (NetworkInterface networkInterface : interfaces) {
+            if (!networkInterface.isUp() || networkInterface.isLoopback() || !networkInterface.supportsMulticast()) {
+                continue;
+            }
+
+            Enumeration<InetAddress> addresses = networkInterface.getInetAddresses();
+            while (addresses.hasMoreElements()) {
+                InetAddress address = addresses.nextElement();
+                if (isIPv6 && address instanceof Inet6Address) {
+                    return networkInterface;
+                }
+
+                if (!isIPv6 && address instanceof Inet4Address) {
+                    return networkInterface;
+                }
+            }
         }
+        return null;
     }
 
-    private static void run(InetAddress groupAddress) {
+    private static void run(InetAddress groupAddress) throws SocketException {
         String myID = UUID.randomUUID().toString();
         Map<NodeInfo, Long> liveNodes = new HashMap<>();
-
+        NetworkInterface networkInterface = findSuitableInterface(groupAddress);
+        if (networkInterface == null) {
+            System.out.println("networkInterface == null");
+        }
+        
     }
 }
